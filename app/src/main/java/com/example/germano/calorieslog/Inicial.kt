54db76a531581c89b98ca7822da2db69015ef02d
germanoo <bgermano89@gmail.com>
2018-11-21 17:40:35 -0300

    tela inicial

---
+++ app/src/main/java/com/example/germano/calorieslog/Inicial.kt
@@ -0,0 +1,12 @@
+package com.example.germano.calorieslog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicial)
+    }
+}
