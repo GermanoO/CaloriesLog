4d1c9e5ec97f7ad77b309c59224c981281dee0b5
germanoo <bgermano89@gmail.com>
2018-11-21 02:06:35 -0300

    tela dados 1 e dados 2

---
+++ app/src/main/java/com/example/germano/calorieslog/Dados.kt
@@ -0,0 +1,12 @@
+package com.example.germano.calorieslog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Dados : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados)
+    }
+}
---
+++ app/src/main/java/com/example/germano/calorieslog/Objetivo.kt
@@ -0,0 +1,15 @@
+package com.example.germano.calorieslog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Objetivo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_objetivo)
+
+
+
+    }
+}
