b819fabe2a7725d114966234e04257e9e4bb42fe
germanoo <bgermano89@gmail.com>
2018-11-20 23:27:31 -0300

    cadastro 1

---
+++ app/src/main/java/com/example/germano/calorieslog/Cadastro.kt
@@ -0,0 +1,12 @@
+package com.example.germano.calorieslog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
